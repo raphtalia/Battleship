59fde93c42d1bb4243a1705cbf32daf5f8e25a93
raphtalia <59540801+raphtalia@users.noreply.github.com>
2022-02-17 09:57:11 -0600

    Refactored `Simulation`

--- src/Game/Simulation.java
+++ src/Game/Simulation.java
@@ -4,51 +4,36 @@ import CLI.Colors;
 import Data.Vector2;
 
 public class Simulation extends Game {
-    // TODO: Figure out if all the max ship counts can be turned into a hashmap
-    public Simulation(int boardWidth, int boardHeight, int maxCarriers, int maxBattleships, int maxCruisers,
-            int maxSubmarines, int maxDestroyers, int maxPatrolBoats) {
-        // TODO: Check if equivalent to Array.prototype.reduce
-        super(boardWidth, boardHeight,
-                maxCarriers + maxBattleships + maxCruisers + maxSubmarines + maxDestroyers + maxPatrolBoats);
-
-        // TODO: If parameters can be a hashmap could loop over hashmap to place ships
-        for (int i = 0; i < maxCarriers; i++) {
-            this.placeShipRandom(new Ship(ShipType.CARRIER));
-        }
-
-        for (int i = 0; i < maxBattleships; i++) {
-            this.placeShipRandom(new Ship(ShipType.BATTLESHIP));
-        }
-
-        for (int i = 0; i < maxCruisers; i++) {
-            this.placeShipRandom(new Ship(ShipType.CRUISER));
-        }
-
-        for (int i = 0; i < maxSubmarines; i++) {
-            this.placeShipRandom(new Ship(ShipType.SUBMARINE));
-        }
-
-        for (int i = 0; i < maxDestroyers; i++) {
-            this.placeShipRandom(new Ship(ShipType.DESTROYER));
-        }
-
-        for (int i = 0; i < maxPatrolBoats; i++) {
-            this.placeShipRandom(new Ship(ShipType.PATROL_BOAT));
-        }
+    public Simulation(int boardWidth, int boardHeight) {
+        super(boardWidth, boardHeight);
     }
 
     public Simulation placeShipRandom(Ship ship) {
         boolean shipPlaced = false;
 
         while (!shipPlaced) {
-            final Vector2 bowLoc = new Vector2((int) (Math.random() * this.getBoardWidth()),
-                    (int) (Math.random() * this.getBoardHeight()));
+            final Vector2 bowLoc = new Vector2((int) (Math.random() * getBoardWidth()),
+                    (int) (Math.random() * getBoardHeight()));
             final Direction dir = Math.random() < 0.5 ? Direction.HORIZONTAL : Direction.VERTICAL;
 
-            if (this.isShipPlacementValid(ship, bowLoc, dir)) {
-                this.placeShip(ship, bowLoc, dir);
+            if (isShipPlacementValid(ship, bowLoc, dir)) {
+                placeShip(ship, bowLoc, dir);
                 shipPlaced = true;
             }
+        }
+
+        return this;
+    }
+
+    public Simulation placeShipRandom(ShipType shipType) {
+        placeShipRandom(new Ship(shipType));
+
+        return this;
+    }
+
+    public Simulation placeShipRandom(ShipType shipType, int numShips) {
+        for (int i = 0; i < numShips; i++) {
+            placeShipRandom(shipType);
         }
 
         return this;
@@ -58,11 +43,11 @@ public class Simulation extends Game {
         boolean shot = false;
 
         while (!shot) {
-            Vector2 target = new Vector2((int) (Math.random() * this.getBoardWidth()),
-                    (int) (Math.random() * this.getBoardHeight()));
+            Vector2 target = new Vector2((int) (Math.random() * getBoardWidth()),
+                    (int) (Math.random() * getBoardHeight()));
 
-            if (!this.isShot(target)) {
-                this.shoot(target);
+            if (!isShot(target)) {
+                shoot(target);
                 shot = true;
             }
         }
@@ -73,10 +58,10 @@ public class Simulation extends Game {
     public Simulation printMap() {
         // Meant for debugging
 
-        for (int y = 0; y < this.getBoardHeight(); y++) {
-            for (int x = 0; x < this.getBoardWidth(); x++) {
+        for (int y = 0; y < getBoardHeight(); y++) {
+            for (int x = 0; x < getBoardWidth(); x++) {
                 final Vector2 loc = new Vector2(x, y);
-                final Ship ship = this.getShipAt(loc);
+                final Ship ship = getShipAt(loc);
 
                 if (ship != null) {
                     if (isShot(loc)) {
@@ -85,7 +70,7 @@ public class Simulation extends Game {
                         System.out
                                 .print(new Colors(ship.getShipType().toString().substring(0, 1) + " ").bold().yellow());
                     }
-                } else if (this.isShot(loc)) {
+                } else if (isShot(loc)) {
                     System.out.print(new Colors("* ").yellow());
                 } else {
                     System.out.print(new Colors(". ").cyan());
